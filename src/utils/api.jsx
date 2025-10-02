6104c4fb6d6bb23242293871459062cc766faece
NguyenTuanBang <bang212006@gmail.com>
2025-10-02 18:44:55 +0700

    Add environment configuration for local and deploy ports

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: `${process.env.VITE_LOCAL_PORT}/api`,
+  baseURL: `${process.env.VITE_DEPLOY_PORT}/api`,
   withCredentials: true,
 });
 
