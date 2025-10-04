53b7f89789348b9409edfe62bfd0d3d89b215c90
NguyenTuanBang <bang212006@gmail.com>
2025-10-04 18:31:41 +0700

    Refactor routing structure in App component to improve readability and organization

--- src/App.jsx
+++ src/App.jsx
@@ -31,27 +31,27 @@ function App() {
       <Route path="/products" element={<Products />} />
       
       <Route path="/products/detail" element={<ProductDetail />} /> */}
-      <Routes>
-        <Route path='/' element={<Products />} />
-        <Route path='/product/:id' element={<ProductDetail />} />
-      
+
+      <Route path='/' element={<Products />} />
+      <Route path='/product/:id' element={<ProductDetail />} />
 
 
-        <Route path="/myaccount" element={<MyAccount />}>
-          <Route index element={<Profile />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="address" element={<Address />} />
-          <Route path="change-password" element={<ChangePassword />} />
-        </Route>
 
-        <Route path="/authen" element={<Authen />}>
-          <Route index element={<Login />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-        </Route>
-      </Routes>
+      <Route path="/myaccount" element={<MyAccount />}>
+        <Route index element={<Profile />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="address" element={<Address />} />
+        <Route path="change-password" element={<ChangePassword />} />
+      </Route>
+
+      <Route path="/authen" element={<Authen />}>
+        <Route index element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+      </Route>
     </Routes>
+
 
   );
 }
